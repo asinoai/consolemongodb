8b88d80cb12849b265df4b0c0b3022ba1c339a60
asinoai <aron.sinoai@linkonomics.com>
2016-07-26 23:46:31 +0300

    Dumping users

--- main/src/main/java/aron/sinoai/consolemongodb/main/UserDumper.java
+++ main/src/main/java/aron/sinoai/consolemongodb/main/UserDumper.java
@@ -1,8 +1,10 @@
 package aron.sinoai.consolemongodb.main;
 
+import com.mongodb.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.net.UnknownHostException;
 import java.sql.*;
 
 /**
@@ -10,7 +12,33 @@ import java.sql.*;
 public class UserDumper {
     private static Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
-    public void dumpUsers() {
+    private PropertiesController propertiesController = new PropertiesController();
+
+    public void dumpUsers() throws UnknownHostException {
+        final MongoClientOptions.Builder builder = new MongoClientOptions.Builder()
+                .connectionsPerHost(150)
+                .cursorFinalizerEnabled(true);
+
+        final String mongoUrl = propertiesController.getMongoUrl();
+        final MongoClientURI mongoClientURI = new MongoClientURI(mongoUrl, builder);
+        final MongoClient mongoClient = new MongoClient(mongoClientURI);
+        final DB database = mongoClient.getDB(mongoClientURI.getDatabase());
+
+        System.out.println("=======================");
+
+        final DBCollection users = database.getCollection("users");
+
+        final DBCursor dbObjects = users.find();
+
+        while (dbObjects.hasNext()) {
+            final DBObject item = dbObjects.next();
+
+            final String userName = (String) item.get("userName");
+            final String password = (String) item.get("password");
+
+            System.out.printf("username : %s; password: %s; \n",  userName, password);
+        }
+        System.out.println("=======================");
 
     }
 
